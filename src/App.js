2d3459caa365bcb804e60f4de558fc959baff93e
04zAM <49489566+04zAM@users.noreply.github.com>
2021-08-12 19:30:24 -0500

    Estilos y entorno globales

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
 
-function App() {
+// React without JSX
+/*const Star = () => React.createElement('div', null, 'This is a litle star *')
+
+const App = () => {
+  return (
+    Star()
+  );
+}*/
+
+// Styles
+import { GlobalStyle } from "./GlobalStyle"
+
+// Components
+const App = () => {
   return (
     <div className="App">
-      Start here.
+      Hola Adri como estas?
+      <GlobalStyle />
     </div>
-  );
-}
+  )
+};
 
 export default App;
